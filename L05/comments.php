257e674f6064607395ca671b83e26a83182e1975
Angey22 <babandval@gmail.com>
2021-02-20 02:26:27 +0200

    Comments examples

---
+++ L05/comments.php
@@ -0,0 +1,29 @@
+<?php
+
+// echo 'taest 01', PHP_EOL;
+# echo 'taest 02', PHP_EOL;
+echo 'taest 03', PHP_EOL;
+/*
+echo 'taest 04', PHP_EOL;
+echo 'taest 05', PHP_EOL;
+*/
+echo 'taest 06', PHP_EOL;
+//echo 'taest 07', PHP_EOL;
+//echo 'taest 08', PHP_EOL;
+//echo 'taest 09', PHP_EOL;
+echo 'taest 10', PHP_EOL;
+
+echo 'taest \n11', PHP_EOL;
+echo "taest \n12", PHP_EOL;
+
+
+/**
+ * Shows an example of API-style comment
+ * @param string $testA
+ * @param int $testB
+ * @return string
+ */
+function testSMTH($testA, $testB)
+{
+    return 'TEST';
+}
